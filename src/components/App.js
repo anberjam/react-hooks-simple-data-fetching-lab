b5a13cf8caaaa2557417ba60442cecd32a61184b
anberjamz <anberjam@yahoo.com>
2021-01-14 19:18:57 -0500

    commit tree 1c5868ebca08a113dca6ca70c81e39a278a4b6f5

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useState, useEffect} from "react"
+
+function App () {
+    const [message , setMessage] = useState("")
+    const [load, setLoad] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(dogPic =>  {
+            setMessage (dogPic.message)
+            setLoad(true)
+        })
+    },[])
+
+    if (load == false) { return <p>Loading...</p>}
+
+    return (
+        <img src={message} alt= "A Random Dog" />
+    )
+}
+
+export default App
+
